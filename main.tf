89fc50481aaceea4cfcdc4fee5dc6527a303588a
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-17 13:01:50 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
 resource "aws_security_group" "main" {
-  name        = "${var.env}-${var.type}-alb"
-  description = "${var.env}-${var.type}-alb"
+  name        = local.name
+  description = local.name
   vpc_id      = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.ingress_ports
-    content {
-      description      = "APP"
-      from_port        = ingress.value["port"]
-      to_port          = ingress.value["port"]
-      protocol         = "tcp"
-      cidr_blocks      = var.sg_cidrs
-    }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidrs
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidrs
   }
 
   egress {
@@ -22,62 +25,53 @@ resource "aws_security_group" "main" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = merge(var.tags, {Name =  "${var.env}-${var.type}-alb"})
+  tags = merge(var.tags, {Name =  local.name})
 }
 
-
 resource "aws_lb" "main" {
-  name               = "${var.env}-${var.type}"
+  name               = local.name
   internal           = var.internal
   load_balancer_type = "application"
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-  tags = merge(var.tags, {Name =  "${var.env}-${var.type}-alb"})
+  tags = merge(var.tags, {Name =  local.name})
 }
-#
-#resource "aws_lb_listener" "main" {
-#  count = var.enable_https ? 0 : 1
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = var.target_group_arn
-#  }
-#}
-#
-#resource "aws_lb_listener" "https" {
-#  count = var.enable_https ? 1 : 0
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "443"
-#  protocol          = "HTTPS"
-#  ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = var.certificate_arn
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = var.target_group_arn
-#  }
-#}
-#
-#resource "aws_lb_listener" "http" {
-#  count = var.enable_https ? 1 : 0
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type = "redirect"
-#
-#    redirect {
-#      port        = "443"
-#      protocol    = "HTTPS"
-#      status_code = "HTTP_301"
-#    }
-#  }
-#}
-#
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "500"
+    }
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  count = var.enable_https ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 #resource "aws_route53_record" "main" {
 #  name    = var.dns_name == null ? "${var.component}-${var.env}" : var.dns_name
 #  type    = "CNAME"
---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "listener_arn" {
+  value = aws_lb_listener.https.arn
+}
+output "alb_name" {
+  value = aws_lb.main.dns_name
+}
